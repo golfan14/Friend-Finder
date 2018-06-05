25341521dfbad8c334661c3bf7ff4a948e9ac959
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-06-04 19:01:53 -0600

    logic

---
+++ app/data/friends.js
@@ -0,0 +1,179 @@
+module.exports =
+
+    [
+        {
+            "name": "Ahmed",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+            "scores": [
+                "5",
+                "1",
+                "4",
+                "4",
+                "5",
+                "1",
+                "2",
+                "5",
+                "4",
+                "1"
+            ]
+        },
+        {
+            "name": "Jacob Deming",
+            "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+            "scores": [
+                "4",
+                "2",
+                "5",
+                "1",
+                "3",
+                "2",
+                "2",
+                "1",
+                "3",
+                "2"
+            ]
+        },
+        {
+            "name": "Jeremiah Scanlon",
+            "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+            "scores": [
+                "5",
+                "2",
+                "2",
+                "2",
+                "4",
+                "1",
+                "3",
+                "2",
+                "5",
+                "5"
+            ]
+        },
+        {
+            "name": "Louis T. Delia",
+            "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+            "scores": [
+                "3",
+                "3",
+                "4",
+                "2",
+                "2",
+                "1",
+                "3",
+                "2",
+                "2",
+                "3"
+            ]
+        },
+        {
+            "name": "Lou Ritter",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+            "scores": [
+                "4",
+                "3",
+                "4",
+                "1",
+                "5",
+                "2",
+                "5",
+                "3",
+                "1",
+                "4"
+            ]
+        },
+        {
+            "name": "Jordan Biason",
+            "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+            "scores": [
+                "4",
+                "4",
+                "2",
+                "3",
+                "2",
+                "2",
+                "3",
+                "2",
+                "4",
+                "5"
+            ]
+        },
+        {
+            "name": "sds",
+            "photo": "sas",
+            "scores": [
+                "2",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1"
+            ]
+        },
+        {
+            "name": "a",
+            "photo": "https://tse2.mm.bing.net/th?id=OIP.B6tW6dlkqclJGr1mEz26cAHaHa&pid=15.1&P=0&w=300&h=300",
+            "scores": [
+                "2",
+                "2",
+                "2",
+                "2",
+                "2",
+                "2",
+                "3",
+                "2",
+                "2",
+                "2"
+            ]
+        },
+        {
+            "name": "dgn",
+            "photo": "https://pokemongolive.com/img/posts/arphotocontest.png",
+            "scores": [
+                "5",
+                "3",
+                "4",
+                "3",
+                "3",
+                "3",
+                "2",
+                "3",
+                "2",
+                "4"
+            ]
+        },
+        {
+            "name": "Bob",
+            "photo": "bob.jpeg",
+            "scores": [
+                "5",
+                "2",
+                "2",
+                "2",
+                "3",
+                "4",
+                "3",
+                "3",
+                "3",
+                "2"
+            ]
+        },
+        {
+            "name": "bgfzgb",
+            "photo": "zfdb",
+            "scores": [
+                "2",
+                "3",
+                "2",
+                "2",
+                "3",
+                "2",
+                "2",
+                "2",
+                "3",
+                "2"
+            ]
+        }]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,31 +2,54 @@ var path = require("path");
 
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    app.post("/api/friends", function(req, res) {
-        var userInput = req.body;
-        var userResponses = userInput.scores;
-        var matchName = "";
-        var matchImage = "";
-        var totalDifference = 5000;
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body);
 
+
+        var newFriend = req.body;
+        // get new friend scores
+        // loop over every friend and accumulate an array of differences
+        //      scores from friend
+        //      loop over every score and accumulate total difference
+        //              calculate diff between the two
+        //              take the absolute value of the diff
+        //              add value to total difference
+        //      store total difference in the array of differences
+        // find the index of the lowest value in the array of differences
+        // insert logic here
+        // return the friend object that sits at that index in the friends array
+
+        var newScores = newFriend.scores.map(parseInt);
+        var scoreDifferences = [];
         for (var i = 0; i < friends.length; i++) {
-            var diff = 0;
-            for (var j = 0; j < userResponses.length; j++) {
-                dif += Math.abs(friends[i].scores[j]-userResponses[j]);
+            var friendScores = friends[i].scores;
+            var totalDifference = 0;
+            for (var j = 0; j < friendScores.length; j++) {
+                totalDifference += Math.abs(newScores[j] - friendScores[j]);
             }
-            if (diff < totalDifference) {
-                totalDifference = diff;
-                matchName = friends[i].name;
-                matchImage = friends[i].photo;
-            }
+            scoreDifferences.push(totalDifference);
         }
-        friends.push(userInput);
+        console.log(scoreDifferences);
 
-        res.json({status: "OK", matchName: matchName, matchImage: matchImage});
+        var bestMatchIndex = 0
+        scoreDifferences.reduce(function (acc, score, index) {
+            if (index === 0) {
+                bestMatchIndex = index;
+                return acc = score;
+            }
+            if (acc > score) {
+                bestMatchIndex = index;
+                return acc = score;
+            }
+        }, 0)
+
+        var bestFriend = friends[bestMatchIndex];
+
+        res.status(200).json(bestFriend);
     });
 };
